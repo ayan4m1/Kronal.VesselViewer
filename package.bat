5141340eeadb3b6bc9c21a95f237c0eece27aee9
ayan4m1 <andrew@bulletlogic.com>
2016-10-17 17:24:40 -0400

    modify packaging script to look like previous releases

--- package.bat
+++ package.bat
@@ -2,6 +2,9 @@
 
 msbuild /t:Build /p:Configuration=Release /p:TargetFramework=v3.5 Kronal.VesselViewer.sln
 mkdir GameData
-xcopy VesselViewer\Resources GameData\ /E
-mkdir GameData\Plugins
-copy VesselViewer\bin\Release\VesselViewer.dll GameData\Plugins\
+mkdir GameData\VesselViewer
+
+xcopy VesselViewer\Resources GameData\VesselViewer /E
+
+mkdir GameData\VesselViewer\Plugins
+copy VesselViewer\bin\Release\VesselViewer.dll GameData\VesselViewer\Plugins\
